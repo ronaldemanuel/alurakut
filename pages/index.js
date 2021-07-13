14a8a557b2c3daa514c90bc356ca19d2f15a3501
Ronald <emronald15el@gmail.com>
2021-07-12 22:22:19 -0300

    grid base and github image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(props) {
+  
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'ronaldVader';
+
+  return (
+    <MainGrid>
+      {/* <Box style="grid-area: profileArea;"> */}
+      <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+        <ProfileSideBar githubUser={githubUser}/>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+        <Box>
+          Bem Vindo
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Pessoas da comunidade
+        </Box>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
